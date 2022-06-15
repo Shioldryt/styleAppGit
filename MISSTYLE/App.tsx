a47fbd79f201a01b747b61696a2f48ab652d3dfa
V <makesmebaldv@gmail.com>
2022-06-15 21:42:18 +0100

    update

--- MISSTYLE/App.tsx
+++ MISSTYLE/App.tsx
@@ -1,12 +1,40 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+//screens
+import HomeScreen from './screens/Home';
+import Clothes from './screens/Clothes';
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator initialRouteName='Register' shifting={true} keyboardHidesNavigationBar={true}>
+      <Tab.Screen 
+      name="Login" 
+      component={HomeScreen}
+      options={{
+        tabBarIcon: 'home',
+        tabBarColor: 'white',
+
+        }}/>
+
+      <Tab.Screen
+       name="Register" 
+       component={Clothes}
+       options={{
+        tabBarIcon: 'wardrobe',
+        tabBarColor: 'white',
+         
+       }}
+       />
+    </Tab.Navigator>
+  </NavigationContainer>
   );
 }
 
